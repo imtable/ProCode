dfae92162889dd3f93bc9cfb17614a590e51457c
imtable <mrelbat@gmail.com>
2021-04-26 16:08:32 +0300

    spider take one

---
+++ hw/calc/js/main.js
@@ -0,0 +1,41 @@
+const btnMultiply = document.querySelector('.btn-multiply');
+const btnDivide = document.querySelector('.btn-divide');
+const btnPow = document.querySelector('.btn-pow');
+const btnSqrt = document.querySelector('.btn-sqrt');
+const result = document.querySelector('.result');
+const inputOne = document.querySelector('.num-1');
+const inputTwo = document.querySelector('.num-2');
+const inputToFixed = document.querySelector('.to-fixed');
+
+const multiply = (arg, arg2) => {
+   return Number(arg.value) * Number(arg2.value);
+}
+const divide = (arg, arg2) => {
+   return Number(arg.value) / Number(arg2.value);;
+}
+const pow = (arg, arg2) => {
+   return Math.pow(Number(arg.value), Number(arg2.value));
+}
+const sqrt = (arg) => {
+   return Math.sqrt(Number(arg.value));
+}
+const fix = (arg, arg2) => {
+   return arg.toFixed(Number(arg2.value));
+}
+
+btnMultiply.addEventListener('click', (ev) => {
+   ev.preventDefault();
+   result.innerHTML = fix(multiply(inputOne, inputTwo), inputToFixed);
+});
+btnDivide.addEventListener('click', (ev) => {
+   ev.preventDefault();
+   result.innerHTML = fix(divide(inputOne, inputTwo), inputToFixed);
+});
+btnPow.addEventListener('click', (ev) => {
+   ev.preventDefault();
+   result.innerHTML = fix(pow(inputOne, inputTwo), inputToFixed);
+});
+btnSqrt.addEventListener('click', (ev) => {
+   ev.preventDefault();
+   result.innerHTML = fix(sqrt(inputOne), inputToFixed);
+});
--- js/main.js
+++ js/main.js
@@ -1,41 +1,125 @@
-const btnMultiply = document.querySelector('.btn-multiply');
-const btnDivide = document.querySelector('.btn-divide');
-const btnPow = document.querySelector('.btn-pow');
-const btnSqrt = document.querySelector('.btn-sqrt');
-const result = document.querySelector('.result');
-const inputOne = document.querySelector('.num-1');
-const inputTwo = document.querySelector('.num-2');
-const inputToFixed = document.querySelector('.to-fixed');
+const gridForm = document.forms.gridForm;
+const gridBlock = document.querySelector('.grid-wrapper');
+const up = document.querySelector('.up');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const down = document.querySelector('.down');
+const spider = document.querySelector('.spider-img');
+let spiderX = 10;
+let spiderY = 10;
 
-const multiply = (arg, arg2) => {
-   return Number(arg.value) * Number(arg2.value);
+const getParam = () => {
+   const data = new FormData(gridForm);
+   const paramRows = Number(data.get('paramRows').trim());
+   const paramCols = Number(data.get('paramCols').trim());
+   return [paramRows, paramCols];
 }
-const divide = (arg, arg2) => {
-   return Number(arg.value) / Number(arg2.value);;
-}
-const pow = (arg, arg2) => {
-   return Math.pow(Number(arg.value), Number(arg2.value));
-}
-const sqrt = (arg) => {
-   return Math.sqrt(Number(arg.value));
-}
-const fix = (arg, arg2) => {
-   return arg.toFixed(Number(arg2.value));
+const setParam = () => {
+   const params = getParam();
+   const paramsCount = params[0] * params[1];
+   const gridCell = `<div class="grid_cell"></div>`;
+   let content = '';
+
+   gridBlock.style.gridTemplateRows = `repeat(${params[0]}, 1fr)`;
+   gridBlock.style.gridTemplateColumns = `repeat(${params[1]}, 1fr)`;
+
+   for (let i = paramsCount; i > 0; i--) {
+      content += gridCell;
+   }
+   gridBlock.innerHTML = content;
 }
 
-btnMultiply.addEventListener('click', (ev) => {
+gridForm.addEventListener('submit', (ev) => {
    ev.preventDefault();
-   result.innerHTML = fix(multiply(inputOne, inputTwo), inputToFixed);
+   gridBlock.innerHTML = '';
+   setParam();
 });
-btnDivide.addEventListener('click', (ev) => {
-   ev.preventDefault();
-   result.innerHTML = fix(divide(inputOne, inputTwo), inputToFixed);
+
+up.addEventListener('mousedown', (ev) => {
+   up.classList.add('pressed');
+   spiderY -= 55;
+   spider.style.top = `${spiderY}px`;
 });
-btnPow.addEventListener('click', (ev) => {
-   ev.preventDefault();
-   result.innerHTML = fix(pow(inputOne, inputTwo), inputToFixed);
+up.addEventListener('mouseup', (ev) => {
+   up.classList.remove('pressed');
 });
-btnSqrt.addEventListener('click', (ev) => {
-   ev.preventDefault();
-   result.innerHTML = fix(sqrt(inputOne), inputToFixed);
+
+left.addEventListener('mousedown', (ev) => {
+   left.classList.add('pressed');
+   spiderX -= 55;
+   spider.style.left = `${spiderX}px`;
+});
+left.addEventListener('mouseup', (ev) => {
+   left.classList.remove('pressed');
+});
+
+right.addEventListener('mousedown', (ev) => {
+   right.classList.add('pressed');
+   spiderX += 55;
+   spider.style.left = `${spiderX}px`;
+});
+right.addEventListener('mouseup', (ev) => {
+   right.classList.remove('pressed');
+});
+
+down.addEventListener('mousedown', (ev) => {
+   down.classList.add('pressed');
+   spiderY += 55;
+   spider.style.top = `${spiderY}px`;
+});
+down.addEventListener('mouseup', (ev) => {
+   down.classList.remove('pressed');
+});
+
+//  keyboard arrows
+
+document.addEventListener('keydown', (ev) => {
+   if (ev.code === 'ArrowUp') {
+      up.classList.add('pressed');
+      spiderY -= 55;
+      spider.style.top = `${spiderY}px`;
+   }
+});
+document.addEventListener('keyup', (ev) => {
+   if (ev.code === 'ArrowUp') {
+      up.classList.remove('pressed');
+   }
+});
+document.addEventListener('keydown', (ev) => {
+   if (ev.code === 'ArrowLeft') {
+      left.classList.add('pressed');
+      spiderX -= 55;
+      spider.style.left = `${spiderX}px`;
+   }
+});
+document.addEventListener('keyup', (ev) => {
+   if (ev.code === 'ArrowLeft') {
+      left.classList.remove('pressed');
+   }
+});
+
+document.addEventListener('keydown', (ev) => {
+   if (ev.code === 'ArrowRight') {
+      right.classList.add('pressed');
+      spiderX += 55;
+      spider.style.left = `${spiderX}px`;
+   }
+});
+document.addEventListener('keyup', (ev) => {
+   if (ev.code === 'ArrowRight') {
+      right.classList.remove('pressed');
+   }
+});
+
+document.addEventListener('keydown', (ev) => {
+   if (ev.code === 'ArrowDown') {
+      down.classList.add('pressed');
+      spiderY += 55;
+      spider.style.top = `${spiderY}px`;
+   }
+});
+document.addEventListener('keyup', (ev) => {
+   if (ev.code === 'ArrowDown') {
+      down.classList.remove('pressed');
+   }
 });
