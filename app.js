370b7e6d5cb771ef6f676820eedbc9a0470d2e62
imtable <mrelbat@gmail.com>
2021-05-29 17:12:54 +0300

    forecast take one

---
+++ app.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const fs = require('fs');
+
+// const url = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=Kyiv&cnt=2&appid=c822ed03c42a3aedc3737fd87e6e881f'; запрос на завтра, но недоступен (видимо, из-за фри-аккаунта)
+const url = 'https://api.openweathermap.org/data/2.5/forecast?id=703448&cnt=40&appid=c822ed03c42a3aedc3737fd87e6e881f&units=metric';
+
+let forecast = '123';
+
+const foo = async () => {
+   axios.get(url).then((response) => {
+      const temp = response.data.list[9].main.temp;
+      const date = response.data.list[9].dt_txt;
+      forecast = `temperature forecast: ${temp} Celsius, date: ${date}`; 
+      console.log(forecast);
+      bar();
+   });
+};
+const bar = async () => {
+   const writeForecast = await fs.promises.appendFile('./forecast.txt', `${forecast} \n`);
+}
+
+foo();
