09d546f1f28626d88146e63f2cb59088600e9c50
imtable <mrelbat@gmail.com>
2021-06-10 18:58:30 +0300

    forecast to hw and getExternalFile take one

--- hw/forecast/app.js
+++ hw/forecast/app.js
@@ -3,23 +3,26 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
    console.log('request from browser');
-
-   // lvl 1
-   // const path = './file.html';
-   // const readStream = fs.createReadStream(path);
-   // readStream.pipe(res);
-   // lvl 1
-
-   const url = 'http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
-   const getFile = () => {
-      // res.setHeader("content-disposition", "attachment"); хедер для скачивания
-      http.get(url, (result) => {
-         result.pipe(res);
-      });  
-   };
-   getFile();
-
+   const path = './file.html';
+   
+   const readStream = fs.createReadStream(path);
+   readStream.pipe(res);
 }).listen(8000);
+
+// lvl3
+// const axios = require('axios');
+// const url = 'https://unsplash.com/photos/AaEQmoufHLk/download?force=true';
+// let webFile = '';
+
+// const getFile = () => {
+//    axios.get(url).then((response) => {
+//       webFile = response.data;
+//       console.log(webFile);
+//    });
+//    return webFile;
+// };
+// getFile();
+// lvl3
 
 // Лвл 1. Поднять сервать на модуле хттп и попробовать получить на него запрос с браузера
 // Лвл 2. Отправить в ответ файл на запрос
