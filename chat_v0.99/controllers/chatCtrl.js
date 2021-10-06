7ce0f5cb790739cd4c82d0548fff2b2f624ae1f0
imtable <mrelbat@gmail.com>
2021-10-06 13:54:46 +0300

    chat v0.99 take one

---
+++ chat_v0.99/controllers/chatCtrl.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const GeneralModel = require('../models/chatUser');
+
+const db = mongoose.connection;
+
+const registerUser = async (name, sid) => {
+  const exist = await GeneralModel.findOne({ userID: sid });
+  if (exist) {
+    await exist.update({ name });
+    return;
+  }
+
+  const item = new GeneralModel;
+  item.name = name;
+  item.userID = sid;
+  const doc = await item.save();
+  console.log(doc._id);
+}
+
+const checkSID = async (sid) => {
+  let data = await GeneralModel.find({ userID: sid });
+  if (data == false) {
+    return 'user not registered';
+  }
+  return data;
+}
+
+module.exports = {
+  registerUser,
+  checkSID
+};
---
+++ chat_v0.99/models/chatUser.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const path = require('path');
+
+const { Schema } = mongoose;
+
+const generalSchema = new Schema({
+   name: {
+      type: Schema.Types.String,
+      maxlength: 100,
+      index: true,
+      required: true
+   },
+   userID: {
+      type: Schema.Types.String,
+      index: true,
+      required: true,
+      unique: true
+   },
+});
+
+const modelName = path.basename(__filename, '.js');
+const model = mongoose.model(modelName, generalSchema);
+
+module.exports = model;
